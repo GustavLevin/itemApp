cd497412f8e18284a6b422f403d71dba697f8dc5
GustavLevin <gustav_levin@hotmail.com>
2024-09-11 17:49:57 +0200

    item test

--- src/app/components/itemsList.js
+++ src/app/components/itemsList.js
@@ -1,25 +1,31 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
-export default function ItemsList() {
+export default function ItemsPage() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchItems = async () => {
-      const response = await fetch("/api/items");
-      const data = await response.json();
-      setItems(data);
+      try {
+        const response = await fetch("/api/items");
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
     };
+
     fetchItems();
   }, []);
 
   return (
     <div>
-      <h2>Items List</h2>
+      <h1>Items List</h1>
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            <h3>{item.name}</h3>
+            <h2>{item.name}</h2>
             <p>{item.description}</p>
             <p>Quantity: {item.quantity}</p>
             <p>Category: {item.category}</p>
